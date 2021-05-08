249fbacc3dbf6fa976933ac173ab18e3c8bed628
Rick Morrow <rclark.morrow@gmail.com>
2021-05-08 13:58:05 -0700

    Implement auth0 provider
    Created Auth0 authentication provider wrapper and implemented in
    index.js

---
+++ frontend/src/auth/index.js
@@ -0,0 +1,8 @@
+// ./src/auth/index.js
+
+// This file imports and exports authorization components
+// and modules.
+import Auth0ProviderWithHistory from 'auth/auth0-provider-with-history';
+
+// Exports.
+export { Auth0ProviderWithHistory };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Local modules.
-import App from './App';
+import App from 'App';
+import { Auth0ProviderWithHistory } from 'auth';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
   </React.StrictMode>,
   document.getElementById('root')
 );
