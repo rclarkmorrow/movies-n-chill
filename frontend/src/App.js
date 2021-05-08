1b1555ea059be72c070a954d2775bbb0b16b56f1
Rick Morrow <rclark.morrow@gmail.com>
2021-05-08 12:58:59 -0700

    Initialize application
    Set up metadata in index.html, add icons and images to public
    directory, clear out unnecessary default react files, index.js and
    app.js stripped down to basics.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// ./src/app.js
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Movies 'n Chill
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+// ./src/index.js
+
+// External package dependencies.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Local modules.
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
