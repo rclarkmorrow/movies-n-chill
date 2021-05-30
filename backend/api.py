665ace66ef83daa713d9b700738ff5bb671883c6
dturco <73612631+dturco@users.noreply.github.com>
2021-05-29 18:13:39 -0700

    Merge with Raj's

--- backend/api.py
+++ backend/api.py
@@ -7,6 +7,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 # from jose import jwt
 from Models import db, Movies, Users, SelectedMovies
+from Matches import Profiles
 
 
 def create_app():
@@ -79,7 +80,6 @@ def create_app():
             new_user = Users.query.filter_by(auth0_id=user['auth0_id']).first()
             print("New user name: " + new_user.user_name)
             print("New user id: " + str(new_user.user_id))
-
 
             print(user['movies'])
             if user['movies']:
@@ -180,30 +180,35 @@ def create_app():
     @app.route('/users/<uid>', methods=['GET', 'PATCH', 'DELETE'])
     def get_patch_or_delete_user(uid):
         print(f'UID is: {uid}')
-        print(f'today: {datetime.date.today()}') # {datetime.date.today()}')
+        print(f'today: {datetime.date.today()}')  # {datetime.date.today()}')
         user = Users.query.filter_by(user_id=uid).first_or_404\
                 (description='There is no data with {}'.format(uid))
+
         if request.method == 'GET':
             # Returned object is a list.
             print(f'usr is type {type(user)}')
             # The items in the list are object of type User
             return jsonify(user.with_movies())
         elif request.method == 'PATCH':
+            print("----- Edit -------")
             req = request.get_json()
             for key, value in req.items():
                 if key != 'movies':
                     setattr(user, key, value)
-            user.modified_date = datetime.date.today()
-            user.modified_by = 'api patch'
-            if user.movies:
-                for movie in user.movies:
+            user['modified_by'] = 'api patch'
+
+            print("Edit user_id: " + str(user['user_id']))
+            selected_movies = [SelectedMovies.query.filter_by(user_id=user['user_id'])]
+            print(selected_movies)
+            if user['movies']:
+                for movie in user['movies']:
                     # NOTE: Create a function to check
                     # the new list of movies and delete
                     # SelectedMovie relationships that
                     # no longer exist, and create new ones.
                     pass
 
-            user.update()
+            # user.update()
             return jsonify('user updated'), 200
         elif request.method == 'DELETE':
             user.delete()
@@ -247,7 +252,21 @@ def create_app():
         # in user ID's movie list matching with other users
         # somehow.
 
-        return jsonify(f'matches for user {uid}'), 200
+        matching_users, matching_percentage = Profiles.get_matching_users(uid)
+        print('matching_user', matching_users)
+
+        musers = []
+        for usr in matching_users:
+            musers.append(Users.query.filter_by(user_id=usr).first())
+
+        count = 0
+        user_to_append = []
+        for user in musers:
+            temp = user.full_for_matches()
+            temp['match_percent'] = matching_percentage[count]
+            user_to_append.append(temp)
+            count = count + 1
+        return jsonify(user_to_append), 200
 
     return app
 
