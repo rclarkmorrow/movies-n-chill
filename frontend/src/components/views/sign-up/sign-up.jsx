6666f109bd9e2d802283d69c76abab1a8f343239
Rick Morrow <rclark.morrow@gmail.com>
2021-06-02 16:26:44 -0700

    Bug fixes
    Fixed a bug in the forms.

--- frontend/src/components/views/sign-up/sign-up.jsx
+++ frontend/src/components/views/sign-up/sign-up.jsx
@@ -2,21 +2,15 @@
 
 // External package dependencies.
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 // Local imports.
 import { UserProfileForm } from 'components';
-import { currentUserSelector } from 'store';
 
 const SignUp = () => {
-  const history = useHistory();
-  const { currentUser } = useSelector(currentUserSelector);
   return (
     <>
       {
-        currentUser ? history.push("/edit-profile")
-        : <UserProfileForm />
+        <UserProfileForm />
       }
     </>
   );
